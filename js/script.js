99d631675bba788d109afa89344f9686b00b5cf7
NexenFull14 <100863416+NexenFull14@users.noreply.github.com>
2023-05-16 11:47:17 +0200

    Added node packages and fixed errors in script.js

--- js/script.js
+++ js/script.js
@@ -1,14 +1,15 @@
 const loginButton = document.getElementById("log-in");
+const animatedWhiteHive = document.getElementById("animated-element");
 
 loginButton.addEventListener("click", () => {
     window.location.href = "../login.html";
 });
 
-const animatedElement = document.getElementById("animated-element");
-
 animate = () => {
-    animatedElement.style.transform = "translateX(20%)";
-    animatedElement.style.opacity = "1";
+    animatedWhiteHive.style.transform = "translateX(20%)";
+    animatedWhiteHive.style.opacity = "1";
 };
 
-window.onload(animate());
+window.onload = () => {
+    animate();
+};
